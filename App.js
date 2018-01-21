402b293fd8fbe1328bac3bcaf54942500140a4ce
fujikawa <fujikawa.naoki@mac.com>
2018-01-21 22:33:15 +0900

    websocketの接続まで

--- App.js
+++ App.js
@@ -45,6 +45,8 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const rpi_host_name = "raspberrypi.local";
 
 const configuration = {"iceServers": [{"url": "stun:" + rpi_host_name + ":3478"}]};
+const socketURL = "wss://" + rpi_host_name + ":8080" + "/stream/webrtc";
+let ws;
 
 export default class App extends Component<{}> {
 
@@ -61,7 +63,45 @@ export default class App extends Component<{}> {
     }
 
     _onPlayPress(event) {
+        console.log("_onPlayPress");
         this.setState({status: 'ready', info: '接続準備中'});
+
+        ws = new WebSocket(socketURL);
+
+        ws.onopen = () => {
+            // connection opened
+            console.log("WebSocket","Connection opened");
+            const request = {
+                what: "call",
+                options: {
+                    force_hw_vcodec: true,
+                    vformat: '60'
+                }
+            };
+            const message = JSON.stringify(request);
+            console.log("WebSocket:call", message);
+            ws.send(message);
+        };
+
+        ws.onmessage = (e) => {
+            // a message was received
+            console.log("WebSocket","a message was received");
+            let received_msg = JSON.parse(e.data);
+            console.log(received_msg);
+        };
+
+        ws.onerror = (e) => {
+            // an error occurred
+            console.log("WebSocket","an error occurred");
+            console.log(e.message);
+        };
+
+        ws.onclose = (e) => {
+            // connection closed
+            console.log("WebSocket","Connection closed");
+            console.log(e.code, e.reason);
+
+        };
 
 
         this.setState({status: 'connected', info: '接続中'});
